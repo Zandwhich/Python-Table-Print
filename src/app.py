775c31a0167f118850e1115091d95e57a06f90b2
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2023-07-24 23:33:53 +1000

    Included some dummy code

---
+++ src/app.py
@@ -0,0 +1,5 @@
+# TODO: Remove this file
+
+from table import print_table
+
+print_table(True, True)
---
+++ src/table.py
@@ -0,0 +1,8 @@
+def print_table(include_column_header: bool, include_row_header: bool) -> None:
+    print('+--------------------------+')
+    print('|        |        |        |')
+    print('+--------------------------+')
+    print('|        |        |        |')
+    print('|        |        |        |')
+    print('|        |        |        |')
+    print('+--------------------------+')
