132dabb67f92dcb2d294e5aff7dfd836b7f3fee5
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2023-11-04 20:31:36 +1300

    Got rid of some old files

--- src/python-table-print/app.py
+++
@@ -1,5 +0,0 @@
-# TODO: Remove this file
-
-from table import print_table
-
-print_table(True, True)
--- src/python-table-print/table.py
+++
@@ -1,8 +0,0 @@
-def print_table(include_column_header: bool, include_row_header: bool) -> None:
-    print("+--------------------------+")
-    print("|        |        |        |")
-    print("+--------------------------+")
-    print("|        |        |        |")
-    print("|        |        |        |")
-    print("|        |        |        |")
-    print("+--------------------------+")
