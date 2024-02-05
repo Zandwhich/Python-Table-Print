d36f8f9ddf038f51385487065eb4d8b150a23373
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2024-02-05 20:14:25 +1300

    First attempt at creating a working test

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,5 +1,20 @@
-# TODO: Add testing for the table
+from ..python_table_print import PrintTable
 
 
 def test_temp():
-    assert True
+    table = PrintTable()
+    
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    assert table.get_table() == """
+    **************************************************************
+    *         Col 1 *              Col 2 *                 Col 3 *
+    **************************************************************
+    *       Entry 1 *     Entry number 2 *          Entry 3 baby *
+    * Another entry *                yay * an entry in the table *
+    *     Fun times * This is kinda cool *                 wooow *
+    **************************************************************
+    """
