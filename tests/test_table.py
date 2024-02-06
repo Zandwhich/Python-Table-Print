3be07eed96fe0d56985b6f16ccb4424761f838d6
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2024-02-06 13:38:55 +1300

    Moved the strucutre around a bit and cfreated the first couple of tests for the basic functions of the table

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,7 +1,14 @@
-from ..python_table_print import PrintTable
+import sys
+import os
+
+# Add the parent directory (project root) to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-def test_temp():
+from src.python_table_print import PrintTable
+
+
+def test_basic_table():
     table = PrintTable()
     
     table.add_row("Col 1", "Col 2", "Col 3")
@@ -9,12 +16,28 @@ def test_temp():
     table.add_row("Another entry", "yay", "an entry in the table")
     table.add_row("Fun times", "This is kinda cool", "wooow")
 
-    assert table.get_table() == """
-    **************************************************************
-    *         Col 1 *              Col 2 *                 Col 3 *
-    **************************************************************
-    *       Entry 1 *     Entry number 2 *          Entry 3 baby *
-    * Another entry *                yay * an entry in the table *
-    *     Fun times * This is kinda cool *                 wooow *
-    **************************************************************
-    """
+    assert table.get_table() == """**************************************************************
+*         Col 1 *              Col 2 *                 Col 3 *
+**************************************************************
+*       Entry 1 *     Entry number 2 *          Entry 3 baby *
+* Another entry *                yay * an entry in the table *
+*     Fun times * This is kinda cool *                 wooow *
+**************************************************************
+"""
+
+def test_basic_no_header():
+    table = PrintTable()
+    
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+    table.has_header_row = False
+    
+    assert table.get_table() == """**************************************************************
+*         Col 1 *              Col 2 *                 Col 3 *
+*       Entry 1 *     Entry number 2 *          Entry 3 baby *
+* Another entry *                yay * an entry in the table *
+*     Fun times * This is kinda cool *                 wooow *
+**************************************************************
+"""
