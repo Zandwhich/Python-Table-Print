b4818a2c7d6de00de90dcb76f16c398519329cf4
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2024-03-22 22:44:15 +1300

    linting

--- tests/test_table.py
+++ tests/test_table.py
@@ -174,16 +174,16 @@ def test_table_different_row_justification():
 
 def test_table_different_column_justification():
     table = PrintTable()
-    
+
     table.add_row("Col 1", "Col 2", "Col 3")
     table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
     table.add_row("Another entry", "yay", "an entry in the table")
     table.add_row("Fun times", "This is kinda cool", "wooow")
-    
+
     table.set_column_justification(0, Justification.LEFT)
     table.set_column_justification(1, Justification.CENTRE)
     table.set_column_justification(2, Justification.RIGHT)
-    
+
     assert (
         table.get_table()
         == """**************************************************************
