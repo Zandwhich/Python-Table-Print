b46ba20d8ee6aa44bd52ff1519444d171e91db80
Alex Z <Zandwhich@users.noreply.github.com>
2024-03-24 13:33:48 +1300

    Justification (#59)
    Adds the ability to justify the entire table, a row, a column, or an individual cell either left, centre, or right.
    
    Closes #50. Closes #52.
    
    
    * Included the enum in there and added a comment about turning the internal storage into a table
    
    * Linting
    
    * Updated teh settings so that self is displayed slightly differently
    
    * First pass at writing the code
    
    * Changed the code to catch the case wehre columns weren't being created
    
    * Included justification to the init thingies
    
    * Included comments on what I need to do next
    
    * Exposed the functions. Made some tests. Updated the example. Still some more work to be done
    
    * Got rid of a comment
    
    * udpated the names of some of the tests to better reflect that they're testing specifically the justification of the rows
    
    * Included Python doco for all of the methods
    
    * Updated to include columns setting the justification as well as lintint
    
    * Fixed the issue with the header row not having a border on the bottom
    
    * linting
    
    * Updated the tests to include testing the columns
    
    * linting
    
    * Updated the notes with what I have left to do
    
    * Updated the TODO comment
    
    * Updated the examples to include a more coherent example of what was being implemented
    
    * Changed a comment slightly
    
    * Fixed up and cleaned up some code
    
    * Linting
    
    * Updated the tests to match changing the default justification to the left justification
    
    * Updated the README
    
    * Got rid of comments that I had already completed

--- src/python_table_print/table.py
+++ src/python_table_print/table.py
@@ -1,78 +1,306 @@
+from enum import Enum
+from math import ceil, floor
+from typing import Sequence
+
+
 BASE_BORDER = "*"
 
 
-class PrintTable:
+class Justification(Enum):
+    """An enum used for justifying text in the table"""
+
+    LEFT = 0
+    CENTRE = 1
+    RIGHT = 2
+
+
+class _Cell:
+    """The basic building block of the table that stores the text and the justification of itself"""
+
+    def __init__(self, text: str, justification: Justification | None = None) -> None:
+        self.justification: Justification | None = justification
+        self.text = text
+
+    def _print_as_left_justified(self, max_length: int, border_character: str) -> str:
+        """Prints out the cell left-justified
+
+        Args:
+            max_length (int): The max length of a cell in this column
+            border_character (str): The character to be used on the border at the sides
+
+        Returns:
+            str: The cell as a string with left justification
+        """
+        return (
+            " "
+            + self.text
+            + (" " * (max_length - len(self.text) + 1))
+            + border_character
+        )
+
+    def _print_as_centre_justified(self, max_length: int, border_character: str) -> str:
+        """Prints out the cell centre-justified
+
+        Args:
+            max_length (int): The max length of a cell in this column
+            border_character (str): The character to be used on the border at the sides
+
+        Returns:
+            str: The cell as a string with centre justification
+        """
+        return (
+            " " * (floor((max_length - len(self.text)) / 2) + 1)
+            + self.text
+            + " " * (ceil((max_length - len(self.text)) / 2) + 1)
+            + border_character
+        )
+
+    def _print_as_right_justified(self, max_length: int, border_character: str) -> str:
+        """Prints out the cell right-justified
+
+        Args:
+            max_length (int): The max length of a cell in this column
+            border_character (str): The character to be used on the border at the sides
+
+        Returns:
+            str: The cell as a string with right justification
+        """
+        return (
+            " " * (max_length - len(self.text) + 1) + self.text + " " + border_character
+        )
+
+    def get_cell_as_string(self, max_length: int, border_character: str) -> str:
+        """Returns the cell in string format with the correct formatting.
+        Note that a justification of None will default to a left justification.
+
+        Args:
+            max_length (int): The max length of a cell in that column.
+            border_character (str): The character to be used on the border at the sides
+
+        Raises:
+            Exception: If there is an incorrect/unsupported justification
+
+        Returns:
+            str: The cell as a string with the correct justicication
+        """
+        # Edge case if there is nothing for this column
+        if max_length == 0:
+            return " " + border_character
+
+        match self.justification:
+            case None:  # Default to left-justified
+                return self._print_as_left_justified(max_length, border_character)
+
+            case Justification.LEFT:
+                return self._print_as_left_justified(max_length, border_character)
+
+            case Justification.CENTRE:
+                return self._print_as_centre_justified(max_length, border_character)
+
+            case Justification.RIGHT:
+                return self._print_as_right_justified(max_length, border_character)
+
+            # TODO: Raise an "Unknown Justification" exception
+            case _:
+                raise Exception
+
+
+class _Column:
+    """An abstraction of the column of the table, which holds information about the column"""
+
     def __init__(self) -> None:
-        self.has_header_row = True
+        self.max_length = 0
 
-        self._max_column_lengths = []
-        self._text = []
 
-    def add_row(self, *row: str) -> None:
-        for col_i in range(0, len(row)):
-            if (
-                len(row[col_i]) > self._max_column_lengths[col_i]
-                if col_i < len(self._max_column_lengths)
-                else -1
-            ):
-                if col_i < len(self._max_column_lengths):
-                    self._max_column_lengths[col_i] = len(row[col_i])
-                else:
-                    self._max_column_lengths += [len(row[col_i])]
+class _Row:
+    """An abstraction of the row of the table, which holds each of the cells in that row of the table"""
 
-        self._text += [row]
+    def __init__(self, *row: str, justification: Justification | None = None) -> None:
+        self.cells: dict[int, _Cell] = {}
+
+        for i in range(0, len(row)):
+            self.cells[i] = _Cell(row[i], justification)
+
+    def set_justification_for_cells(self, justification: Justification | None) -> None:
+        """Sets the justification for all of the cells in the row
+
+        Args:
+            justification (Justification | None): The justification to set the cells to. If `None` is passed in, it sets the justificaiton to the default,
+            which currently is the right justification.
+        """
+        for cell in self.cells.values():
+            cell.justification = justification
+
+    def get_row_as_string(
+        self, border_character: str, max_lengths: Sequence[int]
+    ) -> str:
+        """Returns the row as a string
+
+        Args:
+            border_character (str): The border character that is being used for the table
+            max_lengths (Sequence[int]): The max lengths for all of the columns
+
+        Returns:
+            str: _description_
+        """
+        final_row = border_character
+
+        for i in range(0, len(max_lengths)):
+            if i < len(self.cells):
+                final_row += self.cells[i].get_cell_as_string(
+                    max_lengths[i], border_character
+                )
+
+            else:
+                final_row += " " * (max_lengths[i] + 2) + border_character
+
+        return final_row + "\n"
+
+
+class PrintTable:
+    """The table itself. More documentation to come!"""
+
+    def __init__(self) -> None:
+        self.has_header_row: bool = True
+
+        # TODO: Is a dictionary whose keys are ints essentially a list in Python?
+        self._columns: dict[int, _Column] = {}
+        self._rows: dict[int, _Row] = {}
+        self._border_character = BASE_BORDER
+
+    def _check_and_increase_max_column_length(self, column_i: int, length: int) -> None:
+        """Takes in the column number and the length of the new string at that column and changes the length to the given length if longer, or if that column doesn't yet exist.
+
+        Args:
+            column (int): The column at which this is ocurring
+            length (int): The length of the new string
+        """
+        # If there is no column for this yet, add one
+        if column_i >= len(self._columns):
+            self._columns[column_i] = _Column()
+            self._columns[column_i].max_length = length
+            return
+
+        # Check if this length is longer than the currently stored one. If so, update
+        if length > self._columns[column_i].max_length:
+            self._columns[column_i].max_length = length
 
     def _total_border_length(self) -> int:
+        """Figures out the length of the table
+
+        Returns:
+            int: The length of the table
+        """
         total_border_length = 0
 
-        for col_len in self._max_column_lengths:
-            total_border_length += col_len + 3 if not col_len == 0 else 2
+        for col_len in self._columns.values():
+            total_border_length += (
+                col_len.max_length + 3 if not col_len.max_length == 0 else 2
+            )
 
         return total_border_length + 1
 
     def _get_border_row(self) -> str:
+        """Creates the 'border row', or the top and botom row of the table made up of only border characters
+
+        Returns:
+            str: The border row made up of only border characters
+        """
         return BASE_BORDER * self._total_border_length() + "\n"
 
-    def _get_header(self, header_text: tuple[str, ...]) -> str:
-        header = self._get_row(header_text)
-        return header + self._get_border_row()
+    def _get_header(self, row: _Row, border_character: str) -> str:
+        """Creates the 'header rows', which is the row of text sandwiched between two border rows
 
-    def _get_row(self, columns: tuple[str, ...]) -> str:
-        row = BASE_BORDER
-        for col_i in range(0, len(self._max_column_lengths)):
-            # Edge case if there is nothing for this column
-            if self._max_column_lengths[col_i] == 0:
-                row += " " + BASE_BORDER
-                continue
+        Args:
+            row (_Row): The row of text to sandwich between the border rows
+            border_character (str): The border character
 
-            row += (
-                " "
-                * (
-                    self._max_column_lengths[col_i]
-                    - (len(columns[col_i]) if col_i < len(columns) else 0)
-                    + 1
-                )
-                + (columns[col_i] if col_i < len(columns) else "")
-                + " "
-                + BASE_BORDER
+        Returns:
+            str: The row of text sandwiched between two border rows
+        """
+        return (
+            row.get_row_as_string(
+                border_character,
+                [column.max_length for column in self._columns.values()],
             )
+            + self._get_border_row()
+        )
 
-        return row + "\n"
+    def add_row(self, *row: str) -> None:
+        """Adds another row to the bottom of the table
+
+        Args:
+            row (*str): The next row of text to be added to the table
+        """
+        for col_i in range(0, len(row)):
+            self._check_and_increase_max_column_length(col_i, len(row[col_i]))
+
+        self._rows[len(self._rows)] = _Row(*row)
+
+    def set_table_justification(self, justification: Justification) -> None:
+        """Sets the justification for the whole table. Overrides any previous justification that was set
+
+        Args:
+            justification (Justification): The justification for the table
+        """
+        for row in self._rows.values():
+            row.set_justification_for_cells(justification)
+
+    def set_row_justification(self, row_i: int, justification: Justification) -> None:
+        """Sets the justification for the row in the table. Overrides any previous justification for that row that was set.
+
+        Args:
+            row_i (int): The row to justify.
+            justification (Justification): The justification for the row
+        """
+        self._rows[row_i].set_justification_for_cells(justification)
+
+    def set_column_justification(
+        self, col_i: int, justification: Justification
+    ) -> None:
+        """Sets the justificatoin for the column in the table. Overrides any previous justification for that row that was set.
+
+        Args:
+            col_i (int): The index of the column to change
+            justification (Justification): The justification to set for the cells in that column
+        """
+        for row_i in range(0, len(self._rows)):
+            self.set_cell_justification(row_i, col_i, justification)
+
+    def set_cell_justification(
+        self, row_i: int, column_i: int, justificaiton: Justification
+    ) -> None:
+        """Sets the justification for a specific cell in the table. Overrides any previous justification for that cell that was set.
+
+        Args:
+            row_i (int): The row that the cell is in.
+            column_i (int): The column that the cell is in.
+            justificaiton (Justification): The justification for the cell.
+        """
+        self._rows[row_i].cells[column_i].justification = justificaiton
 
     def get_table(self) -> str:
-        if len(self._text) == 0:
-            # TODO: Throw an error here
-            return "ERROR: No text passed in for the table"
+        """Returns the current table as a string
+
+        Raises:
+            Exception: If there is no data, throws an exception. TODO: Create the actual exception for this.
+
+        Returns:
+            str: The currenct table
+        """
+        if len(self._rows) == 0:
+            # TODO: Throw a "table has no length" exception
+            raise Exception
 
         table = self._get_border_row()
 
         if self.has_header_row:
-            table += self._get_header(self._text[0])
+            table += self._get_header(self._rows[0], self._border_character)
 
-        for r_row in range(1 if self.has_header_row else 0, len(self._text)):
-            table += self._get_row(self._text[r_row])
+        for i in range(1 if self.has_header_row else 0, len(self._rows)):
+            table += self._rows[i].get_row_as_string(
+                self._border_character,
+                [column.max_length for column in self._columns.values()],
+            )
 
         return table + self._get_border_row()
-
-    def print_table(self) -> None:
-        print(self.get_table())
--- tests/test_table.py
+++ tests/test_table.py
@@ -5,7 +5,9 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-from src.python_table_print import PrintTable
+from src.python_table_print import PrintTable, Justification
+
+# Basic Functionality Tests
 
 
 def test_basic_table():
@@ -19,12 +21,33 @@ def test_basic_table():
     assert (
         table.get_table()
         == """**************************************************************
-*         Col 1 *              Col 2 *                 Col 3 *
+* Col 1         * Col 2              * Col 3                 *
 **************************************************************
-*       Entry 1 *     Entry number 2 *          Entry 3 baby *
-* Another entry *                yay * an entry in the table *
-*     Fun times * This is kinda cool *                 wooow *
+* Entry 1       * Entry number 2     * Entry 3 baby          *
+* Another entry * yay                * an entry in the table *
+* Fun times     * This is kinda cool * wooow                 *
 **************************************************************
+"""
+    )
+
+
+def test_extra_columns():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3", "Col 4", "Col 5")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    assert (
+        table.get_table()
+        == """******************************************************************************
+* Col 1         * Col 2              * Col 3                 * Col 4 * Col 5 *
+******************************************************************************
+* Entry 1       * Entry number 2     * Entry 3 baby          *       *       *
+* Another entry * yay                * an entry in the table *       *       *
+* Fun times     * This is kinda cool * wooow                 *       *       *
+******************************************************************************
 """
     )
 
@@ -41,7 +64,32 @@ def test_basic_no_header():
     assert (
         table.get_table()
         == """**************************************************************
+* Col 1         * Col 2              * Col 3                 *
+* Entry 1       * Entry number 2     * Entry 3 baby          *
+* Another entry * yay                * an entry in the table *
+* Fun times     * This is kinda cool * wooow                 *
+**************************************************************
+"""
+    )
+
+
+# Justification Tests
+
+
+def test_table_row_right_justification():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+    table.set_table_justification(Justification.RIGHT)
+
+    assert (
+        table.get_table()
+        == """**************************************************************
 *         Col 1 *              Col 2 *                 Col 3 *
+**************************************************************
 *       Entry 1 *     Entry number 2 *          Entry 3 baby *
 * Another entry *                yay * an entry in the table *
 *     Fun times * This is kinda cool *                 wooow *
@@ -50,23 +98,129 @@ def test_basic_no_header():
     )
 
 
-def test_extra_columns():
+def test_table_row_left_justification():
     table = PrintTable()
 
-    table.add_row("Col 1", "Col 2", "Col 3", "Col 4", "Col 5")
+    table.add_row("Col 1", "Col 2", "Col 3")
     table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
     table.add_row("Another entry", "yay", "an entry in the table")
     table.add_row("Fun times", "This is kinda cool", "wooow")
-    table.has_header_row = True
+
+    table.set_table_justification(Justification.LEFT)
 
     assert (
         table.get_table()
-        == """******************************************************************************
-*         Col 1 *              Col 2 *                 Col 3 * Col 4 * Col 5 *
-******************************************************************************
-*       Entry 1 *     Entry number 2 *          Entry 3 baby *       *       *
-* Another entry *                yay * an entry in the table *       *       *
-*     Fun times * This is kinda cool *                 wooow *       *       *
-******************************************************************************
+        == """**************************************************************
+* Col 1         * Col 2              * Col 3                 *
+**************************************************************
+* Entry 1       * Entry number 2     * Entry 3 baby          *
+* Another entry * yay                * an entry in the table *
+* Fun times     * This is kinda cool * wooow                 *
+**************************************************************
+"""
+    )
+
+
+def test_table_row_centre_justification():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    table.set_table_justification(Justification.CENTRE)
+
+    assert (
+        table.get_table()
+        == """**************************************************************
+*     Col 1     *       Col 2        *         Col 3         *
+**************************************************************
+*    Entry 1    *   Entry number 2   *     Entry 3 baby      *
+* Another entry *        yay         * an entry in the table *
+*   Fun times   * This is kinda cool *         wooow         *
+**************************************************************
+"""
+    )
+
+
+def test_table_different_row_justification():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    table.set_row_justification(0, Justification.LEFT)
+    table.set_row_justification(1, Justification.CENTRE)
+    table.set_row_justification(2, Justification.RIGHT)
+    table.set_row_justification(3, Justification.RIGHT)
+    table.set_row_justification(3, Justification.LEFT)
+
+    assert (
+        table.get_table()
+        == """**************************************************************
+* Col 1         * Col 2              * Col 3                 *
+**************************************************************
+*    Entry 1    *   Entry number 2   *     Entry 3 baby      *
+* Another entry *                yay * an entry in the table *
+* Fun times     * This is kinda cool * wooow                 *
+**************************************************************
+"""
+    )
+
+
+def test_table_different_column_justification():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    table.set_column_justification(0, Justification.LEFT)
+    table.set_column_justification(1, Justification.CENTRE)
+    table.set_column_justification(2, Justification.RIGHT)
+
+    assert (
+        table.get_table()
+        == """**************************************************************
+* Col 1         *       Col 2        *                 Col 3 *
+**************************************************************
+* Entry 1       *   Entry number 2   *          Entry 3 baby *
+* Another entry *        yay         * an entry in the table *
+* Fun times     * This is kinda cool *                 wooow *
+**************************************************************
+"""
+    )
+
+
+def test_table_different_cell_justification():
+    table = PrintTable()
+
+    table.add_row("Col 1", "Col 2", "Col 3")
+    table.add_row("Entry 1", "Entry number 2", "Entry 3 baby")
+    table.add_row("Another entry", "yay", "an entry in the table")
+    table.add_row("Fun times", "This is kinda cool", "wooow")
+
+    table.set_row_justification(0, Justification.LEFT)
+    table.set_row_justification(1, Justification.CENTRE)
+    table.set_cell_justification(1, 2, Justification.RIGHT)
+    table.set_row_justification(2, Justification.RIGHT)
+    table.set_cell_justification(2, 1, Justification.LEFT)
+    table.set_row_justification(3, Justification.RIGHT)
+    table.set_row_justification(3, Justification.LEFT)
+    table.set_cell_justification(3, 2, Justification.CENTRE)
+
+    assert (
+        table.get_table()
+        == """**************************************************************
+* Col 1         * Col 2              * Col 3                 *
+**************************************************************
+*    Entry 1    *   Entry number 2   *          Entry 3 baby *
+* Another entry * yay                * an entry in the table *
+* Fun times     * This is kinda cool *         wooow         *
+**************************************************************
 """
     )
