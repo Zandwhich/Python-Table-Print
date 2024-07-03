acb54e9d8c3dd6da8d847b9197218d4a56dba88a
Alex Z <25352454+Zandwhich@users.noreply.github.com>
2024-07-03 22:29:27 +1000

    Reorganization of `table.py` (#64)
    * split up the main file
    
    * created the suggestions folder
    
    * lint
    
    * fix: fixed import issues
    
    * updated version

---
+++ src/python_table_print/column.py
@@ -0,0 +1,5 @@
+class Column:
+    """An abstraction of the column of the table, which holds information about the column"""
+
+    def __init__(self) -> None:
+        self.max_length = 0
---
+++ src/python_table_print/justification.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Justification(Enum):
+    """An enum used for justifying text in the table"""
+
+    LEFT = 0
+    CENTRE = 1
+    RIGHT = 2
